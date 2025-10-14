fc8dd57bcc66c9afdddbc84a2302833980e4e850
gabriel-apg <gabrielantonioperez12@gmail.com>
2025-10-14 20:45:43 -0300

    menu-mobile

--- js/modules/tooltip.js
+++ js/modules/tooltip.js
@@ -1,41 +1,43 @@
 export default function initTooltip() {
-  const tooltips = document.querySelectorAll("[data-tooltip]");
+  const tooltips = document.querySelectorAll('[data-tooltip]');
 
   tooltips.forEach((item) => {
-    item.addEventListener("mouseover", onMouseOver);
-  });
-
+    item.addEventListener('mouseover', onMouseOver);
+  })
+  
   function onMouseOver(event) {
     const tooltipBox = criarTooltipBox(this);
-
+    
     onMouseMove.tooltipBox = tooltipBox;
-    this.addEventListener("mousemove", onMouseMove);
-    onMouseleave.tooltipBox = tooltipBox;
-    onMouseleave.element = this;
-    this.addEventListener("mouseleave", onMouseleave);
+    this.addEventListener('mousemove', onMouseMove);
+  
+    onMouseLeave.tooltipBox = tooltipBox;
+    onMouseLeave.element = this;
+    this.addEventListener('mouseleave', onMouseLeave);
   }
-
-  const onMouseleave = {
+  
+  const onMouseLeave = {
     handleEvent() {
       this.tooltipBox.remove();
-      this.element.removeEventListener("mouseleave", onMouseleave);
-      this.element.removeEventListener("mousemove", onMouseMove);
-    },
-  };
-
+      this.element.removeEventListener('mouseleave', onMouseLeave);
+      this.element.removeEventListener('mousemove', onMouseMove);
+    }
+  }
+  
   const onMouseMove = {
     handleEvent(event) {
-      this.tooltipBox.style.top = event.pageY + 20 + "px";
-      this.tooltipBox.style.left = event.pageX + 20 + "px";
-    },
-  };
-
+      this.tooltipBox.style.top = event.pageY + 20 + 'px';
+      this.tooltipBox.style.left = event.pageX + 20 + 'px';
+    }
+  }
+  
   function criarTooltipBox(element) {
-    const tooltipBox = document.createElement("div");
-    const text = element.getAttribute("aria-label");
-    tooltipBox.classList.add("tooltip");
+    const tooltipBox = document.createElement('div');
+    const text = element.getAttribute('aria-label');
+    tooltipBox.classList.add('tooltip');
     tooltipBox.innerText = text;
     document.body.appendChild(tooltipBox);
     return tooltipBox;
   }
 }
+
--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
-import initScrollSuave from "./modules/scroll-suave.js";
-import initAnimacaoScroll from "./modules/animacao-scroll.js";
-import initAccordion from "./modules/accordion.js";
-import initTabNav from "./modules/tabnav.js";
-import initModal from "./modules/modal.js";
-import initTooltip from "./modules/tooltip.js";
-import initDropDownMenu from "./modules/dropdown-menu.js";
+import initScrollSuave from './modules/scroll-suave.js';
+import initAnimacaoScroll from './modules/scroll-animacao.js';
+import initAccordion from './modules/accordion.js';
+import initTabNav from './modules/tabnav.js';
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+import initDropdownMenu from './modules/dropdown-menu.js';
+import initMenuMobile from './modules/menu-mobile.js';
 
 initScrollSuave();
 initAnimacaoScroll();
@@ -12,4 +13,5 @@ initAccordion();
 initTabNav();
 initModal();
 initTooltip();
-initDropDownMenu();
+initDropdownMenu();
+initMenuMobile();
