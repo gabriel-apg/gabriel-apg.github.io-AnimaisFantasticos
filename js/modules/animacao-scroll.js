7a76761506cb0e46005daecd7be8217d41d58f45
gabriel-apg <gabrielantonioperez12@gmail.com>
2025-10-16 19:31:20 -0300

    eslint

--- js/modules/animacao-scroll.js
+++ js/modules/animacao-scroll.js
@@ -1,16 +1,17 @@
 export default function initAnimacaoScroll() {
   const sections = document.querySelectorAll('[data-anime="scroll"]');
+
+  function animaScroll() {
+    sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSectionVisible = sectionTop - windowMetade < 0;
+      if (isSectionVisible) section.classList.add("ativo");
+      else section.classList.remove("ativo");
+    });
+  }
+
   if (sections.length) {
     const windowMetade = window.innerHeight * 0.6;
-
-    function animaScroll() {
-      sections.forEach((section) => {
-        const sectionTop = section.getBoundingClientRect().top;
-        const isSectionVisible = sectionTop - windowMetade < 0;
-        if (isSectionVisible) section.classList.add("ativo");
-        else section.classList.remove("ativo");
-      });
-    }
 
     animaScroll();
 
